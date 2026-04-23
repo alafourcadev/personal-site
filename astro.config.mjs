c84c194072298425a296d2a68323809e094f7dbf
Ale <alafourcadedespaigne@gmail.com>
2026-04-23 00:44:50 -0300

    feat: add MDX support and enable components in Day 14 post

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import sitemap from '@astrojs/sitemap'
+import mdx from '@astrojs/mdx'
 
 export default defineConfig({
   site: 'https://alafourca.dev',
   trailingSlash: 'never',
-  integrations: [tailwind({ applyBaseStyles: false }), sitemap()],
+  integrations: [tailwind({ applyBaseStyles: false }), sitemap(), mdx()],
   markdown: {
     shikiConfig: {
       themes: {
